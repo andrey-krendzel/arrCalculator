399ee7632ac320c47b858065b61605086fd91448
user <andreykrendzel@hotmail.com>
2023-01-17 16:47:50 +0200

    added POST endpoint, updated readme

--- README.md
+++ README.md
@@ -1,12 +1,30 @@
 # Endpoints
-The /calculate/{array} endpoind accepts an array, then calculates the missing positive integer from the array.
+* GET /calculate/{array} endpoind accepts an array of integeres, then calculates the missing positive integer from the array and returns it.
 
-For example, *http://localhost:8080/calculate/1,5,3,2,0,1,-3,-6,-4*
+Example 1: *http://localhost:8080/calculate/1,5,3,2,0,1,-3,-6,-4*
 
 Returns 4.
 
-*http://localhost:8080/calculate/5,1,0,4,10,6,-3,-2* 
+Example 2: *http://localhost:8080/calculate/5,1,0,4,10,6,-3,-2* 
 
 Returns 2
 
-There are two tests with MockMVC to these GET endpoints in the test folder. 
+* POST /calculateAlternative/ accepts an array in [] format as the POST body. It returns the missing positive integer. 
+
+Example 1:
+
+![](post_example.png)
+
+Returns 4 just as the first GET request.
+
+Example 2: 
+
+![](POST_example_2.png)
+
+Returns 2 just as the second GET request.
+
+There are two tests with MockMVC to the GET endpoints in the test folder.
+
+# ToDo
+
+- Write two MockMVC tests for the POST endpoint
--- src/main/java/com/arrCalculator/arrCalculator/controller/ArrCalculatorController.java
+++ src/main/java/com/arrCalculator/arrCalculator/controller/ArrCalculatorController.java
@@ -3,9 +3,7 @@ package com.arrCalculator.arrCalculator.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
 import java.util.Arrays;
@@ -37,6 +35,30 @@ class ArrCalculatorController {
 
         return new ResponseEntity<>(missingPositive, HttpStatus.OK);
 
+    }
+
+    @PostMapping("/calculateAlternative")
+    public ResponseEntity<Integer> calculateAlternative(@RequestBody Integer[] array) {
+        int firstPositive = 0;
+        int missingPositive = 999;
+
+        //Go through the array, find smallest positive number. Assume that's the missing positive
+        for (int number : array) {
+            if (number > 0 && number < missingPositive) {
+                missingPositive = number;
+            }
+        }
+
+        while(true){
+            if (Arrays.asList(array).contains(missingPositive)){
+                missingPositive++; //If array contains missingPositive, increase missingPositive int by 1
+            } else {
+                break; //missingPositive is not in the array, break loop, that's the missing int
+            }
+        }
+
+
+        return new ResponseEntity<>(missingPositive, HttpStatus.OK);
     }
 
 
